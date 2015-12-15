d4ebfbed322fa6293c22ca7686dd2cc48d7aedbd
Filipe Carmona <filipecarmona@outlook.com>
2015-12-15 16:37:38 -0200

    ajustes finais + testes no emulador

--- js/main.js
+++ js/main.js
@@ -5,63 +5,90 @@ var dia,
     minutos;
 
 var apodUrl,
-    apodTitulo,
-    apodData;
+    apodTitulo;
+
+function configuracoes(){
+    $.ajaxSetup({
+        url: "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY",
+        success: handleXhr
+    });
+    $.ajax();
+    var strLocale = navigator.locale | "pt-BR";
+    moment.locale(strLocale);
+}
 
 function atualizarHora(){
     //moment -> tela
-    dia = moment().format("ddd, DD/MM");
+    var diaAtual = dia,
+        novoDia = moment().format("DD");
+    dia = moment().format("ddd DD/MM");
     horas = moment().format("HH");
     minutos = moment().format("mm");
     mostrarHora();
-}
-
-function mostrarHora(){
-    document.querySelector("#str_hora").innerText = horas;
-    document.querySelector("#str_minuto").innerText = minutos;
-    document.querySelector("#str_dia").innerText = dia;
-
-    var faltaParaMinuto = 60 - moment().format("s");
-    setTimeout(atualizarHora(),faltaParaMinuto*1000);
-}
-
-function handleXhr(event, xhr, options){
-    //if media type = image then trocaFundo
-    if(xhr.status === 200 && event.media_type === "image"){
-        trocarFundo(event.url);
+    if(novoDia > diaAtual){
+        $.ajax();
     }
 }
 
-function trocarFundo(url){
-    //xhr apod -> fundo
-    document.querySelector("body")style.backgroundImage = "url('"+url+"')";
+function mostrarHora(){
+    document.querySelector("#horas").innerText = horas;
+    document.querySelector("#minutos").innerText = minutos;
+    document.querySelector("#dia").innerText = dia;
+
+    var faltaParaMinuto = 60 - moment().format("s");
+    setTimeout(function(){
+        atualizarHora();
+    },faltaParaMinuto*1000);
+}
+
+function handleXhr(event, xhr, options){
+    if(options.status === 200 && event.media_type === "image"){
+        apodTitulo = event.title;
+        apodUrl = event.url;
+        document.querySelector("body").style.backgroundImage = "url('"+event.url+"')";
+    }
+}
+
+function mostrarTitulo(){
+    alert(apodTitulo);
+}
+
+function setBateria(val){
+    battery_level = val;
 }
 
 function atualizarBateria() {
+    var cor = "green";
     var battery_level = Math.floor(battery.level * 10) + 1;
-    document.querySelector("#batt-val")style.width = battery_level + "%";
+    if(battery_level > 50){
+        cor = "green";
+    }else if(battery_level > 15){
+        cor = "yellow";
+    }else{
+        cor = "red";
+    }
+    document.querySelector("#batt-val").style.width = battery_level + "%";
+    document.querySelector("#batt-val").style.backgroundColor = cor;
 }
 
 function bindEvents() {
-    battery.addEventListener('chargingchange', atualizarBateria);
-    battery.addEventListener('chargingtimechange', atualizarBateria);
-    battery.addEventListener('dischargingtimechange', atualizarBateria);
-    battery.addEventListener('levelchange', atualizarBateria);
-
+    if(battery){
+        battery.addEventListener('chargingchange', atualizarBateria);
+        battery.addEventListener('chargingtimechange', atualizarBateria);
+        battery.addEventListener('dischargingtimechange', atualizarBateria);
+        battery.addEventListener('levelchange', atualizarBateria);
+    }
     // add eventListener for ambientmodechanged
     window.addEventListener('ambientmodechanged', function(e) {
         console.log("ambientmodechanged : " + e.detail.ambientMode);
         if (e.detail.ambientMode === true) {
             // rendering ambient mode case
             //ambientDigitalWatch();
-
         } else {
             // rendering normal case
             //initDigitalWatch();
         }
     });
-}
-window.onload = function() {
     document.addEventListener('tizenhwkey', function(e) {
         if (e.keyName === "back") {
             try {
@@ -69,14 +96,15 @@ window.onload = function() {
             } catch (ignore) {}
         }
     });
-
-    $.ajaxSetup({
-        url: "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY",
-        success: handleXhr
+    $("#data").off("click").on("click",function(){
+        mostrarTitulo();
     });
-
+}
+window.onload = function() {
+    configuracoes();
     atualizarHora();
-    mostrarHora();
+    if(battery){
+        atualizarBateria();
+    }
     bindEvents();
-    
 };
